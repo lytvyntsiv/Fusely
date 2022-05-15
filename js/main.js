7e2d22741c3b101518ddd14c9a14618683414c31
lytvyntsiv <100198094+lytvyntsiv@users.noreply.github.com>
2022-05-15 22:36:36 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,8 @@
+function myFunction () {
+  let link = document.querySelector(".modal-menu");
+  if (link.style.display === "block") {
+    link.style.display = "none";
+  } else {
+    link.style.display = "block";
+  }
+}
